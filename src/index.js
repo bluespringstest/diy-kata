65fc1358ad1c14b7b272a9aa6d812c799cfe1b1d
bluespringstest <71788357+bluespringstest@users.noreply.github.com>
2020-11-13 19:30:21 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const digPow = require("./kata1.digPow");
+const fizzBuzz = require("./kata1.fizzBuzz");
+const booleanToWord = require("./kata2.booleanToWord");
+const numberToReversedDigits = require("./kata3.numberToReversedDigits");
+const humanCatDogYears = require("./kata4.humanCatDogYears");
+const reachDestination = require("./kata5.reachDestination");
+const joinNames = require("./kata6.joinNames");
+const getEmployerRole = require("./kata7.getEmployerRole");
 
 module.exports = {
-  digPow
+  fizzBuzz,
+  booleanToWord,
+  numberToReversedDigits,
+  humanCatDogYears,
+  reachDestination,
+  joinNames,
+  getEmployerRole
 };
